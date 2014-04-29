0145c17938c3580a4d58ff71d2d25af58ab5b8ed
ivijay <vijay.innamuri@gmail.com>
2014-04-30 03:00:10 +0530

    Create script1.py

---
+++ Test1/script1.py
@@ -0,0 +1,55 @@
+#######################################################################################################
+#  Python script to retrive the all YARN application status
+#  
+# Configuration:
+# Current verion of YARN REST API is v1
+# update the host, port in the below script
+#######################################################################################################
+
+import urllib2
+import json
+import urllib
+
+host = "localhost"					# IP address of the job history server
+port = "8088"						# port
+version = "v1"						# version
+resourcePath = "cluster/apps"		# resourcepath
+
+#url = "http://" + host + ":" + port + "/" + "ws" + "/" + version + "/" + resourcePath + "/" + appID
+url = "http://" + host + ":" + port + "/" + "ws" + "/" + version + "/" + resourcePath + "/" 
+u = urllib.urlopen(url)
+
+print("start")
+
+try:
+	resp = urllib2.urlopen(url).read()
+	data = json.loads(resp.decode('utf8'))
+	RestResponse = "success"
+except urllib2.HTTPError, e:
+	#print "HTTP error: %d" % e.code
+	RestResponse = "HTTPError"
+	
+print(RestResponse)
+
+if RestResponse == "success":
+	print("success")
+	#response = data["apps"]["app"][1]["id"]
+	response = data["apps"]["app"]
+	print(id)
+	for i in range(0,len(response)):
+		print(response[i]["id"] +"\t" + response[i]["finalStatus"])
+elif RestResponse == "HTTPError":
+	response = "error"
+	print("error")
+else:
+	progress = 120
+	state = "failure"
+	finalStatus = "Unknown error"
+	print("failure")
+
+	
+	#print(progress)
+	#print(state)
+	#print(finalStatus)
+	
+	#return progress
